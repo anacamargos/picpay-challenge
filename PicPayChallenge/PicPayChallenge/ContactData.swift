588533c991626c3272f4d483520c5fff8f10bee0
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-05 09:53:08 -0300

    Create the ContactData model

---
+++ PicPayChallenge/PicPayChallenge/ContactData.swift
@@ -0,0 +1,16 @@
+//
+//  ContactData.swift
+//  PicPayChallenge
+//
+//  Created by Ana Leticia Camargos on 05/06/20.
+//  Copyright © 2020 Ana Leticia Camargos. All rights reserved.
+//
+
+import Foundation
+
+struct ContactData {
+    let id: Int
+    let name: String
+    let imageURL: URL
+    let username: String
+}
