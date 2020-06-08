bb08a9fea7708dd9f9c8e2943b4643022e8509b1
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-08 09:24:43 -0300

    Request data from URL on `load`

--- PicPayChallenge/PicPayChallengeTests/RemoteContactListLoaderTests.swift
+++ PicPayChallenge/PicPayChallengeTests/RemoteContactListLoaderTests.swift
@@ -8,19 +8,45 @@
 
 import XCTest
 
-final class RemoteContactListLoader {}
+final class RemoteContactListLoader {
+    
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://www.a-url.com.br")!)
+    }
+}
 
-protocol HTTPClient {}
+protocol HTTPClient {
+    func get(from url: URL)
+}
 
 final class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteContactListLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteContactListLoader()
+        _ = RemoteContactListLoader(client: client)
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteContactListLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
