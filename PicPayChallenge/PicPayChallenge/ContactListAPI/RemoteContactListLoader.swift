4ae860e44edee5092ec90887659917ca49635036
Ana Camargos <alcvlara@sga.pucminas.br>
2020-06-10 08:01:26 -0300

    Delivers invalidData error on non 200 HTTP responses

--- PicPayChallenge/PicPayChallenge/ContactListAPI/RemoteContactListLoader.swift
+++ PicPayChallenge/PicPayChallenge/ContactListAPI/RemoteContactListLoader.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteContactListLoader {
     
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
     
     // MARK: - Dependencies
@@ -33,8 +34,12 @@ public final class RemoteContactListLoader {
     // MARK: - Public Methods
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
